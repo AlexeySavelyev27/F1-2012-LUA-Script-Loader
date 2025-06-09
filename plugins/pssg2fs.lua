8035ab4d5d51a35dc44f63f6cb07d0829b61ff12
Alexey Savelyev <promixnaugust@gmail.com>
2025-06-09 13:22:15 +0300

    df

--- plugins/pssg2fs.lua
+++ plugins/pssg2fs.lua
@@ -83,7 +83,8 @@ function OnFrame()
         local base = Memory.GetModuleBase('F1_2012.exe')
         if base and Debug.SetBreakpoint(base + 0x4C3306, 'OnBreakpoint') then
             SCRIPT_RESULT = 'pssg2fs active'
-            writeLog('Breakpoint set at 0x4C2706')
+			writeLog('Base at ' .. base)
+            writeLog('Breakpoint set at 0x4C3306')
             breakpointSet = true
         else
             SCRIPT_RESULT = 'pssg2fs failed'
